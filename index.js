6d6cb5bb4b1487017d3046a7930fa17b705fbd92
Aleksei Ivanov <contact@aleksei.dev>
2024-08-02 23:38:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import crypto from "crypto";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // URL of the API you want to proxy
 const apiEndpoint = "https://finuslugi.ru";
