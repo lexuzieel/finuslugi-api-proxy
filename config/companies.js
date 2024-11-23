20a2df7cf93f8819a831dafe0cacf10c1a84f779
Aleksei Ivanov <contact@aleksei.dev>
2024-11-23 05:59:16 +0300

    Update companies.js

--- config/companies.js
+++ config/companies.js
@@ -1,4 +1,19 @@
 export const companyNameMappings = {
     МАКС: "makc",
+    КАРДИФ: "cardif",
+    АльфаСтрахование: "alfastrah",
+    ВСК: "vsk",
+    Пульс: "puls",
+    "Индивидуальная заявка": "nonsegment",
+    ПАРИ: "skpari",
+    // "Абсолют Страхование": "absolut",
+    "Абсолют Cтрахование": "absolutv2",
+    "Совкомбанк Страхование": "sovcomins",
+    Югория: "yugoria",
+    Росгосстрах: "rgs",
+    "Ренессанс Страхование": "renins",
+    "Зетта Страхование": "zettains",
+    Согласие: "soglasie",
+    ЭНЕРГОГАРАНТ: "energogarant",
 };
 
