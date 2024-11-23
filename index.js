8982bcd3246ee8d517d1e18e9b5b34be78fc127d
Aleksei Ivanov <contact@aleksei.dev>
2024-11-23 03:24:14 +0300

    Format with Prettier

--- index.js
+++ index.js
@@ -15,16 +15,16 @@ const apiEndpoint = "https://finuslugi.ru";
 
 // Use CORS middleware
 app.use((req, res, next) => {
-    const origin = req.get('origin');
+    const origin = req.get("origin");
 
-    const allowedOrigins = process.env.API_CORS_DOMAINS?.split(',') || [
-        'http://localhost',
+    const allowedOrigins = process.env.API_CORS_DOMAINS?.split(",") || [
+        "http://localhost",
     ];
 
     if (!allowedOrigins.includes(origin)) {
-        console.warn(`Request from origin ${origin} not allowed`)
-        res.sendStatus(403)
-        return
+        console.warn(`Request from origin ${origin} not allowed`);
+        res.sendStatus(403);
+        return;
     }
 
     return cors({
@@ -35,33 +35,33 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 const authorizeRequest = (req, res, next) => {
-    const apiKey = process.env.API_KEY || ''
-    const key = req.headers['x-api-key'] || ''
+    const apiKey = process.env.API_KEY || "";
+    const key = req.headers["x-api-key"] || "";
 
     if (apiKey.length > 0 && apiKey != key) {
-        res.sendStatus(401)
+        res.sendStatus(401);
     } else {
-        next()
+        next();
     }
-}
+};
 
 app.use(authorizeRequest);
 
 const normalizeUrl = (url) => {
     if (url.startsWith(process.env.API_PATH_PREFIX)) {
-        return url.replace(process.env.API_PATH_PREFIX, "")
+        return url.replace(process.env.API_PATH_PREFIX, "");
     }
 
-    return url
-}
+    return url;
+};
 
 app.post("*/api/*/partnerLogin", async (req, res) => {
-    const url = normalizeUrl(req.url)
+    const url = normalizeUrl(req.url);
 
-    console.log(req.method, url)
+    console.log(req.method, url);
 
     const headers = {
-        "accept": req.headers["accept"],
+        accept: req.headers["accept"],
         "content-type": req.headers["content-type"],
     };
 
@@ -79,7 +79,8 @@ app.post("*/api/*/partnerLogin", async (req, res) => {
                 ...req.body,
                 email: process.env.FINUSLUGI_API_EMAIL || "",
                 passwordMd5,
-                tokenIsPrivate: process.env.FINUSLUGI_API_PRIVATE_TOKEN == 'true' || false,
+                tokenIsPrivate:
+                    process.env.FINUSLUGI_API_PRIVATE_TOKEN == "true" || false,
             },
         });
 
@@ -96,16 +97,16 @@ app.post("*/api/*/partnerLogin", async (req, res) => {
 });
 
 app.all("*/api/*", async (req, res) => {
-    const url = normalizeUrl(req.url)
+    const url = normalizeUrl(req.url);
 
-    console.log(req.method, url)
+    console.log(req.method, url);
 
     if (req.method != "GET" && req.method != "POST") {
-        res.status(405).send({ error: "Method not allowed" })
+        res.status(405).send({ error: "Method not allowed" });
     }
 
     const headers = {
-        "accept": req.headers["accept"],
+        accept: req.headers["accept"],
         "content-type": req.headers["content-type"],
         authorization: req.headers.authorization,
     };
